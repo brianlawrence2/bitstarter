a55d275f0db4276766bc6226a1ca2bb6718183d7
Brian Lawrence <brianlawrence2@gmail.com>
2013-07-06 00:42:40 -0500

    fixed issue with reading from a buffer

--- web.js
+++ web.js
@@ -5,8 +5,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   var index = fs.readFileSystem;
-  var buffer = new Buffer(index);
-  response.send(buffer.toString('utf-8'));
+  response.send(index.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
