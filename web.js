b479971979bd8179e4d87e71836191899355aa98
Brian Lawrence <brianlawrence2@gmail.com>
2013-07-06 00:44:34 -0500

    fixed issue with reading from a buffer

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var index = fs.readFileSystem;
+  var index = fs.readFileSystem('index.html');
   response.send(index.toString('utf-8'));
 });
 
